4c7aebd970b5be628270999806a76d9e2fbaa7a8
yinqiao2017 <1017023902@qq.com>
2020-04-13 00:35:15 +0800

    3 files modified, 1 added

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -20,4 +20,8 @@ export class HeroFormComponent {
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+  this.model = new Hero(42, '', '');
+}
 }
